3174f4601eddf4839a9d51956d0eaa3cdb2859e4
Harshad Sharma <harshad@sharma.io>
2024-04-16 03:53:12 +0530

    Best-practice: include .sample in repo, ignore .env

--- create-package.sh
+++ create-package.sh
@@ -5,7 +5,7 @@ rm -r pkg/stage
 mkdir -p pkg/stage/usr/local/bin
 mkdir -p pkg/stage/usr/local/etc
 
-cp src/hello.sh     pkg/stage/usr/local/bin/hello
-cp hello.env        pkg/stage/usr/local/etc/hello.env.sample
+cp src/hello.sh         pkg/stage/usr/local/bin/hello
+cp hello.env.sample     pkg/stage/usr/local/etc/hello.env.sample
 
 pkg create -M pkg/manifest.ucl -p pkg/pkg-plist -r pkg/stage -v

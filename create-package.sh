91985f0ed9ada0a1bcf5f33c13f479a1d2ed3e92
Harshad Sharma <harshad@sharma.io>
2024-04-16 01:47:10 +0530

    Reset stage, create dirs, then copy files.

--- create-package.sh
+++ create-package.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-cp src/hello.sh pkg/stage/usr/local/bin/hello
-cp hello.env pkg/stage/usr/local/etc/hello.env.sample
+rm -r pkg/stage
+
+mkdir -p pkg/stage/usr/local/bin
+mkdir -p pkg/stage/usr/local/etc
+
+cp src/hello.sh     pkg/stage/usr/local/bin/hello
+cp hello.env        pkg/stage/usr/local/etc/hello.env.sample
 
 pkg create -M pkg/manifest.ucl -r pkg/stage -p pkg/pkg-plist

8a0cabe5fd42b3510f7c75fcbbf2066323b1a9a2
Harshad Sharma <harshad@sharma.io>
2024-04-16 03:46:26 +0530

    Reformatting

--- create-package.sh
+++ create-package.sh
@@ -8,4 +8,4 @@ mkdir -p pkg/stage/usr/local/etc
 cp src/hello.sh     pkg/stage/usr/local/bin/hello
 cp hello.env        pkg/stage/usr/local/etc/hello.env.sample
 
-pkg create -M pkg/manifest.ucl -r pkg/stage -p pkg/pkg-plist
+pkg create -M pkg/manifest.ucl -p pkg/pkg-plist -r pkg/stage -v
